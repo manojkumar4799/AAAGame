a9c98b0644e9da63b1b5d531c5236cf7c81ad25d
Manoj Kumar Mutukuloju <manoj.mutukuloju18@gmail.com>
2024-07-07 23:28:03 -0400

    Death pose Implemented for relevent death

--- Source/Game/Public/Characeter/CharacterTypes.h
+++ Source/Game/Public/Characeter/CharacterTypes.h
@@ -13,3 +13,12 @@ enum class EActionState :uint8 {
 	EAS_Attacking UMETA(DisplayName = "Attacking"),
 	EAS_Equipping UMETA(DisplayName = "Equipping")
 };
+
+UENUM(BlueprintType)
+enum class EDeathStatus : uint8 {
+	EDS_Alive UMETA(Displayname="alive"),
+	EDS_DeathPose1 UMETA(Displayname = "DeathPose1"),
+	EDS_DeathPose2 UMETA(Displayname = "DeathPose2"),
+	EDS_DeathPose3 UMETA(Displayname = "DeathPose3"),
+	EDS_DeathPose4 UMETA(Displayname = "DeathPose4")
+};
